39990f4eae20468072ee26c602089b5ac533f998
akar-999 <akar@stu.naperville203.org>
2025-11-13 21:09:25 -0600

    Update color mapping and thresholds in SheparFaireyLab
    Adjusted color definitions and threshold logic in SheparFaireyLab.java to use new magenta, yellow, dark purple, and dark orange shades for image processing. Updated comments and reorganized threshold ranges for improved visual output. Also updated SF2.jpg and BlueJ project layout settings.

--- ColorLabs17/SheparFaireyLab.java
+++ ColorLabs17/SheparFaireyLab.java
@@ -19,11 +19,19 @@ public class SheparFaireyLab {
         Color color3 = new Color(0, 25, 51);
         Color color4 = new Color(204, 51, 51);
         
-        Color color5 = new Color(10, 15, 30);
-        Color color6 = new Color(110, 235, 255);
-        Color color7 = new Color(40, 140, 220);
-        Color color8 = new Color(0, 25, 51);
-        Color color9 = new Color(255, 255, 255);
+        Color color5 = new Color(190, 20, 120);   // magenta
+        Color color6 = new Color(255, 220, 60);   // yellow
+        Color color7 = new Color(40, 0, 60);      // dark purple
+        Color color8 = new Color(210, 80, 0);    // dark orange
+        Color color9 = new Color(255, 255, 255);  // white
+
+        
+        //rgb(0,0,0) done
+        //rgb(128,0,255)
+        //rgb(190,92,255)
+        //rgb(0,255,64) done
+        ////rgb(0,0,0) done
+
 
 
 
@@ -73,16 +81,17 @@ public class SheparFaireyLab {
             
             int average = (int)((r+g+b)/3);
             
-                        if (average < 64)
-                Mpixels[i].setColor(color8);
-            else if (average < 128)
-                Mpixels[i].setColor(color7);
-            else if (average < 180)
-                Mpixels[i].setColor(color6);
-            else if (average < 230)
-                Mpixels[i].setColor(color9);
+                         if (average < 60)
+                Mpixels[i].setColor(color7);   // darkest purple
+            else if (average < 110)
+                Mpixels[i].setColor(color5);   // magenta
+            else if (average < 160)
+                Mpixels[i].setColor(color8);   // dark orange
+            else if (average < 210)
+                Mpixels[i].setColor(color6);   // yellow
             else
-                Mpixels[i].setColor(color5);
+                Mpixels[i].setColor(color9);   // white (face highlights)
+            ;
 
                         
 
